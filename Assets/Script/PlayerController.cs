a6e96ca18899f6733bea5789782c6168718c1de1
Alex Wang <y2587wan@edu.uwaterloo.ca>
2018-01-26 23:40:05 -0500

    Rotate shooting point and set up maximum height

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -5,23 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public float rotateSpeed;
     public KeyCode leftKey;
     public KeyCode rightKey;
+    public KeyCode rotateLeft;
+    public KeyCode rotateRight;
     public float force;
     public string playerName;
+    public Transform rotatePoint;
+    public int maxJumpNum = 5;
 
+    private float rotateClockwise = 1;
     private float moveHorizontal = 0;
     private Rigidbody2D rb2d;
+    private Vector3 zAxis;
+    private int jumpCollectCounter = 0;
 
 	void Start () {
-        rb2d = GetComponent<Rigidbody2D>();    	
+        rb2d = GetComponent<Rigidbody2D>();
+        zAxis = new Vector3(0, 0, 1);
 	}
 	
 	void Update () {
         MoveMementControl();
+        RotateShootPoint();
     }
 
-    void MoveMementControl()
+    private void RotateShootPoint()
+    {
+        if (Input.GetKey(rotateLeft))
+            rotateClockwise = -1;
+        else if (Input.GetKey(rotateRight))
+            rotateClockwise = 1;
+        else
+            rotateClockwise = 0;
+        rotatePoint.RotateAround(transform.position, zAxis, rotateSpeed * rotateClockwise);
+    }
+
+    private void MoveMementControl()
     {
         if (Input.GetKey(leftKey))
             moveHorizontal = -1;
@@ -34,8 +55,9 @@ public class PlayerController : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("PowerUp"))
+        if (jumpCollectCounter < maxJumpNum && collision.CompareTag("PowerUp"))
         {
+            jumpCollectCounter++;
             if (rb2d.velocity.y > 0)
                 rb2d.AddForce(new Vector2(0, force));
             else
