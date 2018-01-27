2cba899a1fbe10458d3bedf78f2449a61a55fb15
Alex Wang <y2587wan@edu.uwaterloo.ca>
2018-01-27 17:01:29 -0500

    Shooting trail with collision

--- Assets/Script/LaserController.cs
+++ Assets/Script/LaserController.cs
@@ -4,11 +4,40 @@ using UnityEngine;
 
 public class LaserController : MonoBehaviour {
 
+    public float maxTime = 5f;
     public float maxHeight = 20f;
     public string PlayerName { get; set; }
+    public GameObject laserPoint;
 
-	void Update () {
+    private Vector2 previousPoint;
+    private Vector2 previousSpeed;
+    private float timeCounter = 0;
+    private bool doesNextPointExist = false;
+    private GameObject nextPoint;
+    private void Start()
+    {
+        previousPoint = transform.position;
+        previousSpeed = GetComponent<Rigidbody2D>().velocity;
+    }
+
+    void Update () {
         LaserSelfDestory();
+        timeCounter += Time.deltaTime;
+        if (!doesNextPointExist && timeCounter > maxTime)
+        {
+            nextPoint = (GameObject)Instantiate(laserPoint, previousPoint, transform.rotation);
+            nextPoint.GetComponent<Rigidbody2D>().velocity = previousSpeed;
+            doesNextPointExist = true;
+        }
+
+        if (timeCounter > maxTime)
+        {
+            nextPoint.transform.position = previousPoint;
+            nextPoint.GetComponent<Rigidbody2D>().velocity = previousSpeed;
+            previousSpeed = GetComponent<Rigidbody2D>().velocity;
+            previousPoint = transform.position;
+            timeCounter = 0;
+        }
     }
 
     private void LaserSelfDestory()
