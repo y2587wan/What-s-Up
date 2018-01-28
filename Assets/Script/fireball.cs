b36c15e8fbda4c41d18e191006b724ce1d055734
ftlll <857785996@qq.com>
2018-01-29 04:14:16 +0800

    bg metoer created

---
+++ Assets/Script/fireball.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (5, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
